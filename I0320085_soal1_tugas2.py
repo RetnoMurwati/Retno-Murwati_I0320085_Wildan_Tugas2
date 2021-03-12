858e6adfe89e141a8d81064620ddfada247c94d7
Retno M <murwatiretno2@gmail.com>
2021-03-12 20:23:27 +0700

    Penambahan file exercise2.1-2.4 dan soal 1-2

---
+++ I0320085_soal1_tugas2.py
@@ -0,0 +1,60 @@
+#Menampilkan informasi luas persegi panjang
+print("Menghitung Luas Persegi Panjang")
+
+#Input nilai panjang dan lebar
+p = float(input("Masukkan nilai panjang:"))
+l = float(input("Masukkan nilai lebar:"))
+
+#Menghitung Luas Persegi Panjang
+luas_persegipanjang = p * l
+
+#Menampilkan Hasil Perhitungan
+print("Luas Persegi Panjang adalah", luas_persegipanjang)
+
+#Menampilkan informasi luas lingkaran
+print("Menghitung Luas Lingkaran")
+
+#Input nilai jari-jari
+r = float(input("Masukkan nilai jar-jari:"))
+
+#Menghitung Luas Lingkaran
+luas_lingkaran = 3.14 * (r ** 2)
+
+#Menampilkan Hasil Luas Lingkaran
+print("Luas Lingkaran adalah", luas_lingkaran)
+
+#Menampilkan informasi luas sisi kubus
+print("Menghitung Luas Sisi Kubus")
+
+#Input nilai sisi
+s = float(input("Masukkan nilai sisi:"))
+
+#Menghitung Luas Sisi Kubus
+luas_kubus = 6 * (s ** 2)
+
+#Menampilkan Hasil Luas Sisi Kubus
+print("Luas Sisi Kubus:", luas_kubus)
+
+#Menampilkan informasi konversi suhu celcius ke fahrenheit
+print("Konversi Suhu Celcius ke Fahrenheit")
+
+#Input nilai celcius
+C = float(input("Masukkan nilai celcius:"))
+
+#Menghitung konversi suhu celcius ke fahrenheit
+F = (9/5) * C + 32
+
+#Menampilkan hasil konversi suhu celcius ke fahrenheit
+print("Fahrenheit:", F)
+
+#Menampilkan informasi konversi suhu reamur ke kelvin
+print("Konversi Suhu Reamur ke Kelvin")
+
+#Input nilai reamur
+R = float(input("Masukkan nilai reamur:"))
+
+#Menghitung konversi suhu reamur ke kelvin
+K = (5/4) * R + 273
+
+#Menampilkan hasil konversi suhu reamur ke kelvin
+print("Kelvin:", K)
---
+++ I0320085_soal2_tugas2.py
@@ -0,0 +1,81 @@
+#menampilkan biodata diri
+pembuka = "Assalamu`alaikum wr. wb."
+namaLengkap = "Retno Murwati"
+namaPanggilan = "Retno"
+universitas = "Universitas Sebelas Maret"
+prodi = "Teknik Industri"
+semester = 2
+nim = "I0320085"
+kelas = "C"
+asal = "Kota Magelang"
+tempatTanggalLahir = "Tangerang, 7  Februari 2002"
+tanggalLahir = 7
+bulanLahir = 2
+tahunLahir = 2002
+tanggalSekarang = 11
+bulanSekarang = 3
+tahunSekarang = 2021
+umurTahun = tahunSekarang - tahunLahir
+umurBulan = bulanSekarang-bulanLahir
+umurHari = tanggalSekarang-tanggalLahir
+jenisKelamin = "Perempuan"
+anakKe = 2
+saudara = 2
+sifat = "pendiam dan suka menolong"
+agama = "Islam"
+tinggiBadan = 1.62
+beratBadan = 46.7
+alamat = "Jl Dumpoh Raya, Dumpoh Rt 04 Rw 07, Kelurahan Potrobangsan, Kecamatan Magelang Utara, Kota Magelang"
+riwayatPendidikan = "SMA Negeri 1 Kota Magelang"
+noTelepon = 629527144138
+email = "murwatiretno2@gmail.com"
+sosmed = "instagram dan twitter"
+instagram = "reetn0_"
+twitter = "inisyalRe"
+hobi = "mendengarkan musik dan menonton film"
+genreFilm = "drama, komedi, dan romansa"
+contohFilm = "Be With You"
+hobiLain = "memasak"
+makanan = "nasi goreng, mie goreng, mie rebus, seblak, bakso, sayur oseng, dll"
+laguFavorit = "Peterpan"
+penyanyi = "EXO"
+makananFavorit = "makanan pedas"
+minumanFavorit = "air putih"
+warnaFavorit = "biru laut"
+buahFavorit = "semangka dan melon"
+buahTidakDisukai = "nangka dan durian"
+moto = "Kesuksesan diawali dengan kegagalan, dan kegagalan membawa kesuksesan"
+penutup = "Terima Kasih:)"
+
+print(pembuka)
+print("Perkenalkan nama saya", namaLengkap)
+print("Saya biasa dipanggil", namaPanggilan)
+print("Saat ini saya sedang berkuliah semester", semester, "di", universitas, "Program Studi", prodi)
+print("Saya Lahir di", tempatTanggalLahir)
+print("Umur saya sekarang adalah", umurTahun, "tahun atau lebih tepatnya", umurTahun, "tahun", umurBulan, "bulan", umurHari, "hari")
+print("Agama saya adalah", agama)
+print("Saya merupakan anak ke", anakKe, "dari", saudara, "bersaudara")
+print("Saya merupakan anak yang", sifat)
+print("Saat ini saya tinggal di", asal)
+print("Alamat Rumah saya di", alamat)
+print("Tinggi badan saya adalah", tinggiBadan, "m")
+print("Sedangkan, berat badan adalah", beratBadan, "kg")
+print("Sebelum saya berkuliah di UNS, saya bersekolah di", riwayatPendidikan)
+print("Nomor telepon saya adalah", noTelepon)
+print("Alamat email saya adalah", email)
+print("Selain itu, saya juga memiliku sosial media yaitu", sosmed)
+print("Akun instagram saya adalah", instagram)
+print("Sedangkan, akun twitter saya adalah", twitter)
+print("Hobi saya adalah", hobi)
+print("Genre film yang saya sukai adalah", genreFilm)
+print("seperti", contohFilm)
+print("Lagu yang saya sukai akhir-akhir ini adalah", laguFavorit, "yang dipopulerkan oleh", penyanyi)
+print("Selain itu, saya juga memiliki hobi", hobiLain)
+print("Saya biasa memasak", makanan)
+print("Makanan yang saya sukai adalah", makananFavorit)
+print("Sedangkan, minuman favorit saya adalah", minumanFavorit)
+print("Saya tidak suka dengan buah", buahTidakDisukai)
+print("tetapi saya sangat suka dengan buah", buahFavorit)
+print("Moto hidup saya adalah", moto)
+print(penutup)
+
